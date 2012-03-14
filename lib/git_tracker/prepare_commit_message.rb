12636badbe2a47629f85d34eea5c43ee23cb7a0c
Steven Harman <steveharman@gmail.com>
2012-03-14 17:28:52 -0400

    Ensure all args passed to the hook are captured
    [#26437519]

--- lib/git_tracker/prepare_commit_message.rb
+++ lib/git_tracker/prepare_commit_message.rb
@@ -1,12 +1,15 @@
 module GitTracker
   class PrepareCommitMessage
+    attr_reader :file, :source, :commit_sha
 
     def self.run(file, source=nil, commit_sha=nil)
       new(file, source, commit_sha)
     end
 
-    def initialize(file, source, commit_sha)
-
+    def initialize(file, source=nil, commit_sha=nil)
+      @file = file
+      @source = source
+      @commit_sha = commit_sha
     end
 
   end
--- spec/git_tracker/prepare_commit_message_spec.rb
+++ spec/git_tracker/prepare_commit_message_spec.rb
@@ -9,4 +9,24 @@ describe GitTracker::PrepareCommitMessage do
     end
   end
 
+  describe ".new" do
+    subject { GitTracker::PrepareCommitMessage }
+
+    it "requires the name of the commit message file" do
+      lambda { subject.new }.should raise_error(ArgumentError)
+    end
+
+    it "remembers the name of the commit message file" do
+      subject.new("FILE1").file.should == "FILE1"
+    end
+
+    it "optionally accepts a message source" do
+      hook = subject.new("FILE1", "merge").source.should == "merge"
+    end
+
+    it "optionally accepts the SHA-1 of a commit" do
+      hook = subject.new("FILE1", "commit", "abc1234").commit_sha.should == "abc1234"
+    end
+  end
+
 end
