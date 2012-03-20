3e98f3dcd5e4e09c261097bf9a04aade85137825
Steven Harman <steveharman@gmail.com>
2012-03-20 18:49:37 -0400

    Add very simple detection of PT story number
    [#26437519]

--- lib/git_tracker/commit_message.rb
+++ lib/git_tracker/commit_message.rb
@@ -2,6 +2,12 @@ module GitTracker
   class CommitMessage
 
     def initialize(file)
+      @file = file
+    end
+
+    def contains?(pattern)
+      message = File.read(@file)
+      message.include?(pattern)
     end
 
   end
--- spec/git_tracker/commit_message_spec.rb
+++ spec/git_tracker/commit_message_spec.rb
@@ -6,4 +6,30 @@ describe GitTracker::CommitMessage do
     -> { GitTracker::CommitMessage.new }.should raise_error ArgumentError
   end
 
+  describe "#contains?" do
+    subject { described_class.new(commit_message_file) }
+    let(:commit_message_file) { "COMMIT_EDITMSG" }
+    before do
+      File.stub(:read).with(commit_message_file) { EXAMPLE_COMMIT_EDITMSG }
+    end
+
+    context "commit message contains the special Pivotal Tracker story syntax" do
+      it { subject.should be_contains("[#8675309]") }
+    end
+  end
+
+  EXAMPLE_COMMIT_EDITMSG = <<-EXAMPLE
+Got Jenny's number, gonna' make her mine!
+
+[#8675309]
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# On branch get_jennys_number_#8675309
+# Changes to be committed:
+#   (use "git reset HEAD <file>..." to unstage)
+#
+# new file:   fake_file.rb
+#
+
+EXAMPLE
 end
