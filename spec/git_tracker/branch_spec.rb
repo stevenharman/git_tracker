8fc3944c3eaa32452c148c8dd3a41d1549164067
Steven Harman <steveharman@gmail.com>
2012-03-22 21:58:01 -0400

    Add test for branch without story number
    [#26789779]

--- spec/git_tracker/branch_spec.rb
+++ spec/git_tracker/branch_spec.rb
@@ -3,9 +3,13 @@ require 'git_tracker/branch'
 describe GitTracker::Branch do
   subject { described_class }
 
+  def stub_branch(ref)
+    subject.stub('`') { ref }
+  end
+
   context "Current branch has a story number" do
     before do
-      subject.stub('`') { 'refs/heads/a_very_descriptive_name_#8675309' }
+      stub_branch('refs/heads/a_very_descriptive_name_#8675309')
     end
 
     it "shells out to git, looking for the current HEAD" do
@@ -19,7 +23,10 @@ describe GitTracker::Branch do
   end
 
   context "The current branch doesn't have a story number" do
-    it "finds no story"
+    it "finds no story" do
+      stub_branch('refs/heads/a_very_descriptive_name_without_a_#number')
+      subject.story_number.should_not be
+    end
   end
 
   context "Not on a branch (HEAD doesn't exist)" do
