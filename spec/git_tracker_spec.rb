f2879a3847f506053b3b70b5b34c985682266d3c
Steven Harman <steveharman@gmail.com>
2012-03-22 22:56:58 -0400

    Refactor to more idiomatic Rspec
    [#26437795]

--- spec/git_tracker_spec.rb
+++ spec/git_tracker_spec.rb
@@ -1,17 +1,22 @@
 require 'git_tracker'
 
 describe GitTracker do
+  subject { described_class }
   let(:args) { ["a_file", "the_source", "sha1234"] }
 
   describe ".execute" do
+    before do
+      subject.stub(:prepare_commit_msg) { true }
+    end
+
     it "runs the hook, passing the args" do
-      GitTracker.should_receive(:prepare_commit_msg).with(*args) { true }
-      GitTracker.execute('prepare-commit-msg', *args)
+      subject.should_receive(:prepare_commit_msg).with(*args) { true }
+      subject.execute('prepare-commit-msg', *args)
     end
 
     # TODO: stop the abort from writing to stderr during tests?
     it "doesn't run hooks we don't know about" do
-      lambda { GitTracker.execute('non-existent-hook', *args) }.
+      lambda { subject.execute('non-existent-hook', *args) }.
         should raise_error SystemExit, "[git_tracker] hook: 'non-existent-hook' does not exist."
     end
   end
@@ -19,7 +24,7 @@ describe GitTracker do
   describe ".prepare_commit_msg" do
     it "runs the hook, passing the args" do
       GitTracker::PrepareCommitMessage.should_receive(:run).with(*args) { true }
-      GitTracker.prepare_commit_msg(*args)
+      subject.prepare_commit_msg(*args)
     end
   end
 
