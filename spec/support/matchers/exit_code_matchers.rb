054caa5c7688fa380df77f4bd4e65e3b0ffa97f6
Steven Harman <steveharman@gmail.com>
2012-04-11 11:12:08 -0400

    Add custom RSpec matchers for exit codes

---
+++ spec/support/matchers/exit_code_matchers.rb
@@ -0,0 +1,33 @@
+require 'rspec/expectations'
+
+RSpec::Matchers.define :succeed do
+  actual = nil
+
+  match do |block|
+    begin
+      block.call
+    rescue SystemExit => e
+      actual = e.status
+    end
+    actual and actual == successful_exit_code
+  end
+
+  failure_message_for_should do |block|
+    "expected block to call exit(#{status_code}) but exit" +
+      (actual.nil? ? " not called" : "(#{actual}) was called")
+  end
+
+  failure_message_for_should_not do |block|
+    "expected block not to call exit(#{status_code})"
+  end
+
+  description do
+    "expect block to call exit(#{status_code})"
+  end
+
+  def successful_exit_code
+    0
+  end
+
+end
+
