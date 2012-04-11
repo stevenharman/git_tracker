d7479a029eca83cfff797e232db8b0af3ed53065
Steven Harman <steveharman@gmail.com>
2012-04-11 11:27:49 -0400

    Fix broken exit code matcher
    I forgot to finish renaming status_code to successful_exit_code.

--- spec/support/matchers/exit_code_matchers.rb
+++ spec/support/matchers/exit_code_matchers.rb
@@ -13,16 +13,16 @@ RSpec::Matchers.define :succeed do
   end
 
   failure_message_for_should do |block|
-    "expected block to call exit(#{status_code}) but exit" +
+    "expected block to call exit(#{successful_exit_code}) but exit" +
       (actual.nil? ? " not called" : "(#{actual}) was called")
   end
 
   failure_message_for_should_not do |block|
-    "expected block not to call exit(#{status_code})"
+    "expected block not to call exit(#{successful_exit_code})"
   end
 
   description do
-    "expect block to call exit(#{status_code})"
+    "expect block to call exit(#{successful_exit_code})"
   end
 
   def successful_exit_code
